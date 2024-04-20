69d15da1e415718c4e56de31d40b6b4b78910a90
VictorLuis123 <150992051+VictorLuis123@users.noreply.github.com>
2024-04-20 11:05:40 -0400

    annadida funcionalidad al servivcio, solo falta inyectaro y probar que funcione

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Método para enviar el nombre de la tabla como parámetro al archivo PHP
+  getData(tableName: string): Observable<any> {
+    // URL del archivo PHP que se comunicará con la base de datos
+    const url = 'http://localhost/ServidorEtecsa/mostrar_tablas.php?tableName=${tableName}';
+    
+    // Realizar la solicitud HTTP GET al archivo PHP
+    return this.http.get(url);
+  }
 }
