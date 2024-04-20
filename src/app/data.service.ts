bdffa24f8a75beaa3dce3f986da6f426aefd52cd
VictorLuis123 <150992051+VictorLuis123@users.noreply.github.com>
2024-04-20 12:58:56 -0400

    Establecio conexion con la base de datos, falta imprimir todos los registros en la tavba cliente

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,11 +12,7 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   // Método para enviar el nombre de la tabla como parámetro al archivo PHP
- /*  getData(tableName: string):any{
-    // URL del archivo PHP que se comunicará con la base de datos
-    const url = 'http://localhost/ServidorEtecsa/mostrar_tablas.php?tableName=${tableName}';
-    
-    // Realizar la solicitud HTTP GET al archivo PHP
-    return this.http.get<any>(url);
-  } */
+   getData(tableName: string):any{
+    return this.http.get<any>('http://localhost/ServidorEtecsa/mostrar_tablas.php', { params: { nombre_tabla: tableName } });
+  } 
 }
--- src/app/tablas/tablas.component.ts
+++ src/app/tablas/tablas.component.ts
@@ -7,11 +7,26 @@ import { DataService } from '../data.service';
   styleUrls: ['./tablas.component.css']
 })
 export class TablasComponent implements OnInit{
-  constructor(private dataservice:DataService) { }
-  //tabla='client'
-  ngOnInit():void{
-
+  constructor(private dataService:DataService) { }
+  nombre_tabla='client'
+  elementos:any[]=[]
+  ngOnInit(): void {
+    // Llamar al método del servicio y pasar el nombre de la tabla como parámetro
+    this.dataService.getData(this.nombre_tabla).subscribe((data:any) => {
+      //console.log(data); // Manejar los datos recibidos aquí
+      data.forEach((item:any) => {
+        this.elementos.push(item);
+      });
+    });
   }
+
+  BuscarTabla(tabla:string):void{
+    this.nombre_tabla=tabla;
+    this.elementos=[]
+    this.ngOnInit()
+  }
+
+  
 
  
 }
