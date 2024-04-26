829a004499d30b0a1ba2a31f67a3c081eaf34862
VictorLuis123 <150992051+VictorLuis123@users.noreply.github.com>
2024-04-26 00:12:32 -0400

    Si manda los datos al php

--- src/app/consultas/consultas.component.ts
+++ src/app/consultas/consultas.component.ts
@@ -31,18 +31,18 @@ export class ConsultasComponent implements OnInit{
   }
 
 
-  // submitFormClient(user_data:{name:string, id:string,address:string,telephone:string}){
-  //   console.log(user_data)
-  //   this.dataservice.addUser(user_data).subscribe(
-  //     response => {
-  //       console.log(response);
-  //       alert('Nuevo usuario agregado correctamente');
-  //      },
-  //      error =>{
-  //       alert('No se ha podido ingresar a usuario');
-  //      }
-  //   );
-  // }
+  submitFormClient(user_data:{name:string, id:string,address:string,telephone:string}){
+    console.log(user_data)
+    this.dataservice.addUser(user_data).subscribe(
+      response => {
+        console.log(response);
+        alert('Nuevo usuario agregado correctamente');
+       },
+       error =>{
+        alert('No se ha podido ingresar a usuario');
+       }
+    );
+  }
   
 
 
